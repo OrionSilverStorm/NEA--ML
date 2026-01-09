fde7bb6fb5f7d989d2e2927e48365ab838c5bab7
OrionSilverStorm <lordshiven.s8@gmail.com>
2026-01-09 16:58:41 +0000

    Added export to pdf and text, added a label and text box to enter the name you want the file to be; added an external js script that creates new pdfs

--- Frontend/OutputPage/export.js
+++ Frontend/OutputPage/export.js
@@ -26,12 +26,19 @@ async function copyToClipBoard(){
 
 
 //export as a pdf
-
+function txtToPDF(){
+    const doc = new window.jspdf.jsPDF();
+    doc.text(`${textArea.textContent}`, 10, 10);
+    doc.save(`${fileName.value}.pdf`);
+}
 
 //export as a .txt file
-const downloadFile = () =>{
-    const link = document.createElement("a");
-    const file = new Blob([content], {type: "text/plain"});
-    link.href = URL.createObjectURL(file);
-    link.download = "${}"
+const exportAsTxt = () =>{  //use arrow func as we want to "return" the file to the user implicitly, instead of utright returning it
+    console.log("yes");
+    const link = document.createElement("a");   //create HTML link element which will be attached to the button
+    const file = new Blob([textArea.textContent], {type: "text/plain"}); //get text and add it to the plain-text blob (file like object to represent data not native to js like text files)
+    link.href = URL.createObjectURL(file);  //add the blog to the href attribute of the link, by creating a URL link that points to a given file (blob in this acse)
+    link.download = `${fileName.value}.txt`; //adds the name of the file to the download attribute so it knows this is a downloadble link
+    link.click();   //so that the file is saved on the local computer
+    URL.revokeObjectURL(link.href); //remove the file link at the end to reset the downloadable text file, ready for the next time they edit the editiable text field
 }
