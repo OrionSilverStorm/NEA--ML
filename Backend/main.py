c90d3c881713776944835896c1557996a3598d8f
Shiven K <lordshiven.s8@gmail.com>
2026-01-23 12:05:53 +0000

    Fleshed out backend

---
+++ Backend/main.py
@@ -0,0 +1,9 @@
+#handle files:
+'''
+Use FASTAPI as the backend server
+
+1. recieve file stack from frontend, sync progress with loading bar
+2. call img preproccessing func and preproccess the images
+3. call inference func, infer images
+4. return the infered text back to the frontend
+'''
