5d7375e19bf4864ba449049776e43025acaa89b1
OrionSilverStorm <lordshiven.s8@gmail.com>
2026-01-26 22:29:59 +0000

    added try and except statement, drag and drop w/ mult files leads to internal server error and permission error

--- Backend/main.py
+++ Backend/main.py
@@ -33,9 +33,12 @@ app.add_middleware(
 #decorator to use the HTTP POST protocol into the defined path in the param
 @app.post("/uploadfile/")
 async def Create_Upload_File(file_uploads: list[UploadFile]):  #async funtion that will take in a list of files from the frontend
-    for file_upload in file_uploads:    #iterate over each file to save it to our directory
-        data = await file_upload.read() #will read the file and store its contents in data, use await to make function wait for the file to be read
-        save_to = UPLOAD_DIR / file_upload.filename #save a file in the predefined directory with the files name
-        with open(save_to, 'wb') as f: #write to the files bytes
-            f.write(data)   #write the read data, so we have completley stored this file in our backend
+    try:
+        for file_upload in file_uploads:    #iterate over each file to save it to our directory
+            data = await file_upload.read() #will read the file and store its contents in data, use await to make function wait for the file to be read
+            save_to = UPLOAD_DIR / file_upload.filename #save a file in the predefined directory with the files name
+            with open(save_to, 'wb') as f: #write to the files bytes
+                f.write(data)   #write the read data, so we have completley stored this file in our backend
+    except Exception as e:
+        return {f"{e}"}
     return {"filenames": [f.filename for f in file_uploads]}   #returns a list of file names of all uploaded files
