33ea2a6e6518b16fdcbcb0b2f1004a8a1fffc671
OrionSilverStorm <lordshiven.s8@gmail.com>
2026-01-26 20:00:31 +0000

    Started backend, up to storing uploads (unfinished)

--- Backend/main.py
+++ Backend/main.py
@@ -7,10 +7,24 @@ Use FASTAPI as the backend server
 3. call inference func, infer images
 4. return the infered text back to the frontend
 '''
+
+#to import FastAPI backend and upload file functionality 
 from fastapi import FastAPI, UploadFile
+#to store files in the server
+from pathlib import Path
 
-app = FastAPI()
+#const has the directory that the uplaoded file will go
+UPLOAD_DIR = Path() / 'uploads'
 
+
+app = FastAPI() #instantiate the fast api object
+
+#decorator to use the HTTP POST protocol into the defined path in the param
 @app.post("/uploadfile/")
-async def CreateUploadFile(file: UploadFile):
-    return {"filename": file.filename}
+async def Create_Upload_File(file_uploads: list[UploadFile]):  #async funtion that will take in a list of files from the frontend
+    for file_upload in file_uploads:    #iterate over each file to save it to our directory
+        data = await file_upload.read() #will read the file and store its contents in data, use await to make function wait for the file to be read
+        save_to = UPLOAD_DIR / file_upload.filename #save a file in the predefined directory with the files name
+        with open(save_to, 'wb') as f: #write to the files bytes
+            f.write(data)   #write the read data, so we have completley stored this file in our backend
+    return {"filenames": [f.filename for f in file_uploads]}   #returns a list of file names of all uploaded files
